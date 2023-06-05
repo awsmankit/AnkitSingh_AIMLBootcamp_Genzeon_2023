8c60c792182a168407e261d32ce323ae8d13427a
Ankitkumar Singh <ankitkumar.singh@genzeon.com>
2023-06-05 17:19:32 +0530

    qrcode generator

---
+++ Demo.py
@@ -0,0 +1,19 @@
+# first python program
+# input() will get input in console
+# Then print() will print the wassup and the input word
+
+#print("wassup?" + " " + input("What is you name"))
+
+# print number of letters in a word
+
+# s = input("Enter the Word ")
+# print(len(s))
+# print(len(input("Enter The Word ")))
+
+a= 5
+b= 100
+c= a
+a= b
+b= c
+print(a)
+print(b)
